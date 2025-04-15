40b0a1deeba7b594b225a21dc8729ac0942ee7eb
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-04-15 16:49:16 +0300

    Initial commit

---
+++ src/main/java/ru/zdk/limer/LimerApplication.java
@@ -0,0 +1,13 @@
+package ru.zdk.limer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zdk/limer/LimerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zdk.limer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
